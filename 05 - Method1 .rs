a7cbffa3eb91eec3e6e01fae4cc2af509a9f53e6
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-06-03 00:25:54 +0530

    Create 05 - Method1 .rs

---
+++ 05 - Method1 .rs
@@ -0,0 +1,14 @@
+fn print_country(ptr: &String) {
+    println!("This is {}", ptr);
+}
+
+fn main() {
+    // TODO - Assign a value to a variable
+    let name = String::from("India");
+    // TODO - Use References to print name as many times as we do
+    print_country(&name); // Call a function with a variable as an argument
+    print_country(&name); // Call a function with a variable as an argument again
+}
+
+
+
